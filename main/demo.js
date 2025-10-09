8496ea3835041df36e84c492616a87979ed713ce
chukwujek <nwachukwuchukwujekwu@gmail.com>
2025-10-09 00:48:38 -0700

    fix:update games

---
+++ main/demo.js
@@ -0,0 +1,22 @@
+const todos = [
+   {
+     id:1,
+     text: 'Take out the trash',
+     isCompleted: true
+   },
+   {
+     id:2,
+     text: 'call the boss',
+     isCompleted: true
+   }, 
+   {
+     id:3,
+     text: 'play game',
+     isCompleted: false
+   },
+];
+
+// for
+for(let i = 0; i <= todos.length; i++){
+    console.log(`For Loop Number: ${i}`);
+}   
